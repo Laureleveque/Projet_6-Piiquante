0822f9185c0c4cc8b1c5bd2396addf7a5dd0918e
Laurence Leveque <laureleveque@free.fr>
2022-05-15 21:28:35 +0200

    modifs

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,8 +6,9 @@ const jwt = require("jsonwebtoken"); // importation du package de création et d
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1]; // récupération du token (2ème élément du tableau)
-    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET"); // la méthode verify permet de vérifier la validité d'un token
+    const token = req.headers.authorization.split(" ")[1]; // on extrait le token du header authorization
+    // la méthode verify permet de vérifier la validité d'un token
+    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
     const userId = decodedToken.userId; // récupération de l'userId
     // si userId différent du userId
     if (req.body.userId && req.body.userId !== userId) {
